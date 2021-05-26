14988816432c183864b62e62fac9e06e878761bf
darthinvader <psaradakisj@gmail.com>
2021-05-26 15:48:04 +0300

    [feat] prettier added

---
+++ 02AdvancedFundamentals/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+
+// if (hasDriversLicense) console.log("I can drive");
+
+function logger() {}
+('hello');
