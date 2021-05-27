61a38db837e715f9fb1aae5b96deab651ace8e6a
darthinvader <psaradakisj@gmail.com>
2021-05-27 16:37:52 +0300

    [feat] guess my number finished

--- 06_DOMBasics/script.js
+++ 06_DOMBasics/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+document.querySelector('.number').textContent = '?';
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,13 +14,23 @@ document.querySelector('.check').addEventListener('click', function () {
     return;
   }
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔No Number!';
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too high';
-
     if (score == 1) {
       document.querySelector('.message').textContent = 'You lost the game!';
     }
@@ -36,3 +46,16 @@ document.querySelector('.check').addEventListener('click', function () {
 
   document.querySelector('.score').textContent = score;
 });
+
+document.querySelector('.again').addEventListener('click', again);
+
+function again() {
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing ...';
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+}
