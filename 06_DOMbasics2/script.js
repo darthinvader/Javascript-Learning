3e840431eff4daff1c2c72d8b4bf9223b03645c4
darthinvader <psaradakisj@gmail.com>
2021-05-27 17:18:22 +0300

    [feat] open and close modal

--- 06_DOMbasics2/script.js
+++ 06_DOMbasics2/script.js
@@ -4,6 +4,20 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 
+overlay.addEventListener('click', closeModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {}
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
