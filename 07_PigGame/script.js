dcbaa470d1b6af1275e1109561a0db90e756d6c7
darthinvader <psaradakisj@gmail.com>
2021-05-28 12:57:52 +0300

    [feat] rolling dice functionality

--- 07_PigGame/script.js
+++ 07_PigGame/script.js
@@ -1,10 +1,43 @@
 'use strict';
 
+// Selecting the elements
+
+// Scores
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0Ek = document.getElementById('current--0');
+const current1Ek = document.getElementById('current--1');
 
+// Dice
 const diceEl = document.querySelector('.dice');
+
+// Buttons
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1 Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2 Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  console.log(dice);
+  // Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0Ek.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
+});
