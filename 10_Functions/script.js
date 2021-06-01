9b9ef848afc602d66d9845704bed86ff879ca1cb
darthinvader <psaradakisj@gmail.com>
2021-06-01 13:46:38 +0300

    [learnig] returning functions

--- 10_Functions/script.js
+++ 10_Functions/script.js
@@ -52,33 +52,48 @@
 // newPassport(jonas);
 // console.log(jonas);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original string ${str}`);
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string ${str}`);
 
-  console.log(`Transformed string: ${fn(str)}`);
+//   console.log(`Transformed string: ${fn(str)}`);
 
-  console.log(`Transformed by: ${fn.name}`);
-};
+//   console.log(`Transformed by: ${fn.name}`);
+// };
 
-transformer('Javascript is the best', upperFirstWord);
+// transformer('Javascript is the best', upperFirstWord);
 
-transformer('Javascript is the best', oneWord);
+// transformer('Javascript is the best', oneWord);
 
-// JS uses callbacks all the time
-const high5 = function () {
-  console.log('👋');
-};
+// // JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('👋');
+// };
 
-document.body.addEventListener('click', high5);
+// document.body.addEventListener('click', high5);
 
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+const greeter = greet('Hello');
+greeter('John');
+greeter('Jonas');
+greeter('Steven');
+greeter('Mike');
+
+greet('Hello')('Smettman');
