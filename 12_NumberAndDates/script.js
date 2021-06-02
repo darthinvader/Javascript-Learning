58fdeebdf36c5090e95464b1ff44d5ecb9ff67e7
darthinvader <psaradakisj@gmail.com>
2021-06-02 18:58:05 +0300

    [learning] Number is Nan infinite integer parse

--- 12_NumberAndDates/script.js
+++ 12_NumberAndDates/script.js
@@ -251,3 +251,40 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// Converted
+console.log(+'23');
+console.log(Number('23'));
+
+// Parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e30px', 10));
+
+console.log(Number.parseFloat('2.5rem'));
+console.log(Number.parseInt('2.5rem'));
+console.log(Number.parseInt('       2.5rem   '));
+console.log(parseInt('2.5rem'));
+console.log(parseFloat('2.5rem'));
+
+console.log(Number.isNaN(20));
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN('20'));
+
+console.log(20 / 0);
+console.log(Number.isNaN(20 / 0));
+
+// Good way to check if value is a number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
+
+// Check
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
