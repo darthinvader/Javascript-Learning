3ecb764bbb2facd8eb0e3b06e2f48a297f47acf1
darthinvader <psaradakisj@gmail.com>
2021-06-03 15:13:36 +0300

    [learning] dates arithmetic

--- 12_NumberAndDates/script.js
+++ 12_NumberAndDates/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-06-01T17:01:17.194Z',
+    '2021-06-02T23:36:17.929Z',
+    '2021-06-03T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -82,6 +82,15 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // Functions
 
 const dateFormat = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
   const day = `${date.getDate()}`.padStart(2, 0);
   const month = `${date.getMonth() + 1}`.padStart(2, 0);
   const year = date.getFullYear();
@@ -409,3 +418,11 @@ containerApp.style.opacity = 1;
 
 // future.setFullYear(2040);
 // console.log(future);
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+console.log(calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 17)));
