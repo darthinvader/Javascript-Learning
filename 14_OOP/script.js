6f20bc05fc1d592f0c87953c717065dc284e7a7b
darthinvader <psaradakisj@gmail.com>
2021-06-07 20:35:36 +0300

    [learning] prototype chain

--- 14_OOP/script.js
+++ 14_OOP/script.js
@@ -46,3 +46,22 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+
+const arr = [1, 2, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+const h1 = document.querySelector('h1');
+console.log(h1.__proto__);
+console.dir(x => x + 1);
