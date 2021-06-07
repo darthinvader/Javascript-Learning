1b720ae87a185439bbfa5608bfd1afe3cadc99ae
darthinvader <psaradakisj@gmail.com>
2021-06-07 19:30:21 +0300

    [learning] function constructors

--- 14_OOP/script.js
+++ 14_OOP/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const person1 = new Person('Jonas', 1991);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+const jay = 'Jay';
+
+console.log(person1 instanceof Person);
