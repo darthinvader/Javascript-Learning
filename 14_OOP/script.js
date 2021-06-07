d57e609c826728752366b75d58f46db16f8301ab
darthinvader <psaradakisj@gmail.com>
2021-06-07 20:07:19 +0300

    [learning] prototype

--- 14_OOP/script.js
+++ 14_OOP/script.js
@@ -12,7 +12,7 @@ const Person = function (firstName, birthYear) {
   // };
 };
 
-const person1 = new Person('Jonas', 1991);
+const jonas = new Person('Jonas', 1991);
 
 // 1. New {} is created
 // 2. function is called, this = {}
@@ -25,4 +25,24 @@ console.log(matilda, jack);
 
 const jay = 'Jay';
 
-console.log(person1 instanceof Person);
+console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+
+console.log(jonas.__proto__ == Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+console.log(Person.prototype === jonas.__proto__);
+console.log(matilda.__proto__ === jonas.__proto__);
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
