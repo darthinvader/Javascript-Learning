750f08c71a5dce018050e3eedc40b7e3cb44c8e2
darthinvader <psaradakisj@gmail.com>
2021-06-07 21:06:42 +0300

    [learning] js classes

--- 14_OOP/script.js
+++ 14_OOP/script.js
@@ -97,3 +97,38 @@ car2.brake();
 car2.brake();
 console.log(car);
 console.log(car2);
+
+// Class expression
+// const PersonCl = class {};
+// Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes re executed in strict mode
