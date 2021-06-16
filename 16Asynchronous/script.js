0245bd2e75356f20f32d6763cb33e0abdc885ce4
darthinvader <psaradakisj@gmail.com>
2021-06-16 18:35:04 +0300

    [learning] The Event Loop

--- 16Asynchronous/script.js
+++ 16Asynchronous/script.js
@@ -144,3 +144,16 @@ const getCountryData = function (country) {
 btn.addEventListener('click', function () {
   getCountryData('australia');
 });
+
+console.log('Test Start');
+
+setTimeout(() => console.log('0 sec timer'), 0);
+
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 10000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
