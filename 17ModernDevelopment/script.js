29bec97192a48629d87d9d4292bcbf1827b43182
darthinvader <psaradakisj@gmail.com>
2021-06-19 10:59:34 +0300

    [learning] module pattern

--- 17ModernDevelopment/script.js
+++ 17ModernDevelopment/script.js
@@ -11,8 +11,37 @@
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart);
 
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
-console.log(cart);
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 5);
+// add('apples', 4);
+// console.log(cart);
+
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 123;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to the cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
+console.log(shoppingCart2);
