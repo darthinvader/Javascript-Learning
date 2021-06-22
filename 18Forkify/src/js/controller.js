0783950eb7f5ad5860a38c3ad610f781b4c51c0a
darthinvader <psaradakisj@gmail.com>
2021-06-22 17:43:46 +0300

    [learning] custom hash id

--- 18Forkify/src/js/controller.js
+++ 18Forkify/src/js/controller.js
@@ -31,11 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    console.log(id);
+
+    if (!id) return;
     // Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8b7'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if (!res.ok) {
@@ -158,4 +162,7 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+// showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
